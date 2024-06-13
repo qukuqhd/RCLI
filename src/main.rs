29a02c99ea786e83921d0c8379187c4b042fa1b8
gangx <gangx6906@gmail.com>
2024-06-14 00:39:36 +0800

    添加了命令行的解析

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+// csv 功能： rcli csv -i input.csv -o output.json --header -d ","
+
+use clap::Parser;
+
+//CLI 命令
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about, long_about = None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+#[derive(Debug, Parser)]
+enum SubCommand {
+    //命令行
+    #[command(name = "csv", about = "Show CSV,or convert CSV to other format")]
+    Csv(CsvOpts), //csv功能的cli选项
+}
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    //定义了Csv功能的选项
+    #[arg(short,long,value_parser = verify_input_file)]
+    input: String, //输入的文件路径
+    #[arg(short, long, default_value = "output.json")]
+    output: String, //输出的文件路径默认为output.json
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char, //文件的分割符号默认为,
+    #[arg(long, default_value_t = true)]
+    header: bool, //文档是否有header默认有
+}
 fn main() {
-    println!("Hello, rust!");
+    let opts = Opts::parse(); //解析传入的命令行
+    println!("{:?}", opts);
+}
+//检查输入的文件路径的合法性
+fn verify_input_file(file_name: &str) -> Result<String, String> {
+    if std::path::Path::new(file_name).exists() {
+        Ok(file_name.into())
+    } else {
+        Err("Input file not exist.".into())
+    }
 }
