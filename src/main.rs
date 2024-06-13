b7e809bad72aee6225303143256e510feb6682dc
gangx <gangx6906@gmail.com>
2024-06-14 01:58:08 +0800

    完成了格式转换的通用性

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,7 @@
 use clap::Parser;
 use csv::Reader;
 use rcli::opt::*;
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Player {
-    #[serde(rename = "Name")]
-    name: String,
-    #[serde(rename = "Position")]
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    #[serde(rename = "Nationality")]
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
 fn main() {
     let opts = Opts::parse(); //解析传入的命令行，根据我们定义的数据结构来进行参数合法性的判断
                               //匹配不同可能的命令行选项
@@ -25,10 +11,16 @@ fn main() {
         SubCommand::Csv(option) => {
             let mut reader = Reader::from_path(option.input).unwrap();
             //反序列化得到对象列表
-            let ret = reader
-                .deserialize()
-                .map(|record| record.unwrap())
-                .collect::<Vec<Player>>();
+            let headers = reader.headers().unwrap().clone();
+            let ret: Vec<serde_json::Value> = reader
+                .records()
+                .map(|record| {
+                    headers
+                        .iter()
+                        .zip(record.unwrap().iter())
+                        .collect::<serde_json::Value>()
+                })
+                .collect();
             let json = serde_json::to_string_pretty(&ret).unwrap();
             std::fs::write(option.output, json).unwrap();
         }
